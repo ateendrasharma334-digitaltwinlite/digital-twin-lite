c7fa2942b2d7a481e23a4f38f59099db21677109
Atee <ateendrasharma334@gmail.com>
2026-06-06 13:06:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import streamlit as st
 import pandas as pd
+import networkx as nx
 import matplotlib.pyplot as plt
 import plotly.express as px
 from simulator import forecast_energy, detect_anomalies
@@ -23,7 +24,6 @@ import paho.mqtt.client as mqtt
 import json
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import networkx as nx
 
 mqtt_data = {}
 
