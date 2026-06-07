b3e6bd761d8bb024b67325c03945d86f77d83b0f
Atee <ateendrasharma334@gmail.com>
2026-06-07 11:34:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -82,6 +82,7 @@ def generate_sensor_data():
 # -------------------------------
 # Database Utilities
 # -------------------------------
+DB_PATH = "digital_twin.db"
 
 def get_connection():
     return sqlite3.connect(DB_PATH, check_same_thread=False, timeout=30)
